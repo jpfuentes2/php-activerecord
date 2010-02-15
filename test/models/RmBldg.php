0593f4d8e2f1607e6ca606350a1015cd05aa8e12
Kien La <la.kien@gmail.com>
2010-02-14 23:58:12 -0500

    validators were in wrong format

--- test/models/RmBldg.php
+++ test/models/RmBldg.php
@@ -4,30 +4,30 @@ class RmBldg extends ActiveRecord\Model
 	static $table = 'rm-bldg';
 
 	static $validates_presence_of = array(
-		array('property' => 'space_out', 'message' => 'is missing!@#'),
-		array('property' => 'rm_name')
+		array('space_out', 'message' => 'is missing!@#'),
+		array('rm_name')
 	);
 
 	static $validates_length_of = array(
-		array('property' => 'space_out', 'within' => array(1, 5)),
-		array('property' => 'space_out', 'minimum' => 9, 'too_short' => 'var is too short!! it should be at least %d long')
+		array('space_out', 'within' => array(1, 5)),
+		array('space_out', 'minimum' => 9, 'too_short' => 'var is too short!! it should be at least %d long')
 	);
 
 	static $validates_inclusion_of = array(
-		array('property' => 'space_out', 'in' => array('jpg', 'gif', 'png'), 'message' => 'extension %s is not included in the list'),
+		array('space_out', 'in' => array('jpg', 'gif', 'png'), 'message' => 'extension %s is not included in the list'),
 	);
 
 	static $validates_exclusion_of = array(
-		array('property' => 'space_out', 'in' => array('jpeg'))
+		array('space_out', 'in' => array('jpeg'))
 	);
 
 	static $validates_format_of = array(
-		array('property' => 'space_out', 'with' => '/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i' )
+		array('space_out', 'with' => '/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i' )
 	);
 
 	static $validates_numericality_of = array(
-		array('property' => 'space_out', 'less_than' => 9, 'greater_than' => '5'),
-		array('property' => 'rm_id', 'less_than' => 10, 'odd' => null)
+		array('space_out', 'less_than' => 9, 'greater_than' => '5'),
+		array('rm_id', 'less_than' => 10, 'odd' => null)
 	);
 }
 ?>
