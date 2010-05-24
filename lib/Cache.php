313f5493a5db699177c273305068971ceb697add
Kien La <la.kien@gmail.com>
2010-05-24 18:28:17 -0400

    initial caching classes

---
+++ lib/Cache.php
@@ -0,0 +1,38 @@
+<?php
+namespace ActiveRecord;
+use Closure;
+
+/**
+ * Cache::get('the-cache-key', function() {
+ *   # this gets executed when cache is stale
+ *   return "your cacheable datas";
+ * });
+ */
+class Cache
+{
+	static $adapter = null;
+
+	public static function initialize($url)
+	{
+		$url = parse_url($url);
+		$file = ucwords(Inflector::instance()->camelize($url['scheme']));
+		$class = "ActiveRecord\\$file";
+		require_once dirname(__FILE__) . "/cache/$file.php";
+		static::$adapter = new $class($url);
+	}
+
+	public static function flush()
+	{
+		if (static::$adapter)
+			static::$adapter->flush();
+	}
+
+	public static function get($key, $closure)
+	{
+		if (!($value = static::$adapter->read($key)))
+			static::$adapter->write($key,($value = $closure()));
+
+		return $value;
+	}
+}
+?>
