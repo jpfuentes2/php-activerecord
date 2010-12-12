87fa5c0ab08d5723f6adcbd6453e9e68dc3f586b
Jacques Fuentes <jpfuentes2@gmail.com>
2010-12-12 13:37:35 -0500

    GH-91: get_connection(null) should always return default

--- test/ConnectionManagerTest.php
+++ test/ConnectionManagerTest.php
@@ -11,7 +11,7 @@ class ConnectionManagerTest extends DatabaseTest
 		$this->assert_not_null(ConnectionManager::get_connection(null));
 		$this->assert_not_null(ConnectionManager::get_connection());
 	}
-
+    
 	public function test_get_connection()
 	{
 		$this->assert_not_null(ConnectionManager::get_connection('mysql'));
@@ -24,5 +24,17 @@ class ConnectionManagerTest extends DatabaseTest
 
 		$this->assert_same($a,ConnectionManager::get_connection('mysql'));
 	}
+
+    public function test_gh_91_get_connection_with_null_connection_is_always_default()
+    {
+        $conn_one = ConnectionManager::get_connection('mysql');
+        $conn_two = ConnectionManager::get_connection();
+        $conn_three = ConnectionManager::get_connection('mysql');
+        $conn_four = ConnectionManager::get_connection();
+
+        $this->assert_same($conn_one, $conn_three);
+        $this->assert_same($conn_two, $conn_three);
+        $this->assert_same($conn_four, $conn_three);
+    }
 }
 ?>
