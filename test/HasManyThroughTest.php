3fa59fe7c0cda26af169c8b0b4f13de27356536a
Yoan Blanc <yoan@dosimple.ch>
2010-12-17 11:36:18 +0100

    `include` also fails on `Model::find()` afaik.

--- test/HasManyThroughTest.php
+++ test/HasManyThroughTest.php
@@ -6,13 +6,32 @@ use foo\bar\biz\User;
 use foo\bar\biz\Newsletter;
 
 class HasManyThroughTest extends DatabaseTest {
-    public function test_gh101_has_many_through() {
-        $user = User::find(1);
-        $newsletter = Newsletter::find(1);
+	public function test_gh101_has_many_through() {
+		$user = User::find(1);
+		$newsletter = Newsletter::find(1);
 
-        $this->assert_equals($newsletter->id, $user->newsletters[0]->id);
-        $this->assert_equals('foo\bar\biz\Newsletter', get_class($user->newsletters[0]));
-        $this->assert_equals($user->id, $newsletter->users[0]->id);
-        $this->assert_equals('foo\bar\biz\User', get_class($newsletter->users[0]));
-    }
+		$this->assert_equals($newsletter->id, $user->newsletters[0]->id);
+		$this->assert_equals(
+			'foo\bar\biz\Newsletter',
+			get_class($user->newsletters[0])
+		);
+		$this->assert_equals($user->id, $newsletter->users[0]->id);
+		$this->assert_equals(
+			'foo\bar\biz\User',
+			get_class($newsletter->users[0])
+		);
+	}
+
+	public function test_gh101_has_many_through_include() {
+		$user = User::find(1, array(
+			'include' => array(
+				'usernewsletters' => array('newsletter')
+			)
+		));
+
+		$this->assert_equals(1, $user->id);
+	}
 }
+
+# vim: ts=4 noet nobinary
+?>
--- test/helpers/foo.php
+++ test/helpers/foo.php
@@ -3,25 +3,28 @@
 namespace foo\bar\biz;
 
 class User extends \ActiveRecord\Model {
-    static $has_many = array(
-        array('usernewsletters', 'class_name' => '\foo\bar\biz\Usernewsletter'),
-        array('newsletters', 'class_name' => '\foo\bar\biz\Newsletter',
-              'through' => 'usernewsletters')
-    );
+	static $has_many = array(
+		array('usernewsletters', 'class_name' => '\foo\bar\biz\Usernewsletter'),
+		array('newsletters', 'class_name' => '\foo\bar\biz\Newsletter',
+		      'through' => 'usernewsletters')
+	);
 
 }
 
 class Newsletter extends \ActiveRecord\Model {
-    static $has_many = array(
-        array('usernewsletters', 'class_name' => '\foo\bar\biz\Usernewsletter'),
-        array('users', 'class_name' => '\foo\bar\biz\User',
-              'through' => 'usernewsletters')
-    );
+	static $has_many = array(
+		array('usernewsletters', 'class_name' => '\foo\bar\biz\Usernewsletter'),
+		array('users', 'class_name' => '\foo\bar\biz\User',
+		      'through' => 'usernewsletters')
+	);
 }
 
 class Usernewsletter extends \ActiveRecord\Model {
-    static $belong_to = array(
-        array('user', 'class_name' => '\foo\bar\biz\User'),
-        array('newsletter', 'class_name' => '\foo\bar\biz\Newsletter'),
-    );
+	static $belong_to = array(
+		array('user', 'class_name' => '\foo\bar\biz\User'),
+		array('newsletter', 'class_name' => '\foo\bar\biz\Newsletter'),
+	);
 }
+
+# vim: ts=4 noet nobinary
+?>
