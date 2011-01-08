5f6dfd1a1ada85fcfa6772ead65a9a4008f7ef41
Benjamin <benjamin@github.com>
2011-01-08 14:07:56 +0100

    Gh103: Relax specification of sqlite connection strings.

--- lib/Connection.php
+++ lib/Connection.php
@@ -138,16 +138,24 @@ abstract class Connection
 	 * protocol://username:password@unix(/some/file/path)/dbname
 	 * </code>
 	 *
+	 * Sqlite has a special syntax, as it does not need a database name or user authentication:
+	 *
+	 * <code>
+	 * sqlite://file.db
+	 * sqlite://../relative/path/to/file.db
+	 * sqlite://unix(/absolute/path/to/file.db)
+	 * sqlite://windows(c:/absolute/path/to/file.db) // TODO currently not implemented
+	 * </code>
+	 *
 	 * @param string $connection_url A connection URL
 	 * @return object the parsed URL as an object.
 	 */
 	public static function parse_connection_url($connection_url)
 	{
 		$url = @parse_url($connection_url);
-
-		if (!isset($url['host']))
-			throw new DatabaseException('Database host must be specified in the connection string.');
-
+		if (!$url['host'])
+			throw new DatabaseException('Database host must be specified in the connection string. If you want to specify absolute filenames, use e.g. sqlite://unix(/path/to/file)');
+		
 		$info = new \stdClass();
 		$info->protocol = $url['scheme'];
 		$info->host		= $url['host'];
@@ -155,17 +163,24 @@ abstract class Connection
 		$info->user		= isset($url['user']) ? $url['user'] : null;
 		$info->pass		= isset($url['pass']) ? $url['pass'] : null;
 
+		$allow_blank_db = ($info->protocol == 'sqlite');
 		if ($info->host == 'unix(')
 		{
 			$socket_database = $info->host . '/' . $info->db;
 
-			if (preg_match_all('/^unix\((.+)\)\/(.+)$/', $socket_database, $matches) > 0)
+			if ($allow_blank_db)
+				$unix_regex = '/^unix\((.+)\)\/?().*$/';
+			else
+				$unix_regex = '/^unix\((.+)\)\/(.+)$/';
+			if (preg_match_all($unix_regex, $socket_database, $matches) > 0)
 			{
 				$info->host = $matches[1][0];
 				$info->db = $matches[2][0];
 			}
 		}
-
+		if ($allow_blank_db && $info->db)
+			$info->host .= '/' . $info->db;
+		
 		if (isset($url['port']))
 			$info->port = $url['port'];
 
