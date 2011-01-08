55dab2f951ee6f40545e06b9956ab85af33531d3
Benjamin <benjamin@github.com>
2011-01-08 14:15:29 +0100

    Spaces

--- lib/Connection.php
+++ lib/Connection.php
@@ -153,10 +153,10 @@ abstract class Connection
 	public static function parse_connection_url($connection_url)
 	{
 		$url = @parse_url($connection_url);
-		
+
 		if (!isset($url['host']))
 			throw new DatabaseException('Database host must be specified in the connection string. If you want to specify an absolute filename, use e.g. sqlite://unix(/path/to/file)');
-		
+
 		$info = new \stdClass();
 		$info->protocol = $url['scheme'];
 		$info->host		= $url['host'];
