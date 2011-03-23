04fb58587cb52ebc4f386581abceb1edff8125c9
Jacques Fuentes <jpfuentes2@gmail.com>
2011-03-22 20:30:44 -0400

    use default port for Memcache if none has been specified

--- lib/cache/Memcache.php
+++ lib/cache/Memcache.php
@@ -3,11 +3,25 @@ namespace ActiveRecord;
 
 class Memcache
 {
+	const DEFAULT_PORT = 11212;
+
 	private $memcache;
 
+	/**
+	 * Creates a Memcache instance.
+	 *
+	 * Takes an $options array w/ the following parameters:
+	 *
+	 * <ul>
+	 * <li><b>host:</b> host for the memcache server </li>
+	 * <li><b>port:</b> port for the memcache server </li>
+	 * </ul>
+	 * @param array $options
+	 */
 	public function __construct($options)
 	{
 		$this->memcache = new \Memcache();
+		$options['port'] = isset($options['port']) ? $options['port'] : self::DEFAULT_PORT;
 
 		if (!$this->memcache->connect($options['host'],$options['port']))
 			throw new CacheException("Could not connect to $options[host]:$options[port]");
