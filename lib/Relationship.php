93efbe2acf7709e5e6ea792afd7f44879eaf23e8
Yoan Blanc <yoan@dosimple.ch>
2011-05-04 23:34:35 +0200

    Fix count on non-saved relations

--- lib/Relationship.php
+++ lib/Relationship.php
@@ -699,7 +699,7 @@ class RelatedObjects implements \ArrayAccess, \Countable, \IteratorAggregate {
 	 */
 	public function count() {
 		if (!isset($this->_count)) {
-			if (!isset($this->_data)) {
+			if (!isset($this->_data) && $this->_options) {
 				$class_name = $this->_class_name;
 				$options = $this->_options;
 				if (isset($options['offset']))
