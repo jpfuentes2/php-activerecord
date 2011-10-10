e2e14b207e6c6ff571d5e100c79f0776a350f72a
Doug.G.R <gontijo.douglas@gmail.com>
2011-10-10 14:18:17 -0300

    Moving Exceptions.php to the top

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -7,6 +7,7 @@ define('PHP_ACTIVERECORD_VERSION_ID','1.0');
 if (!defined('PHP_ACTIVERECORD_AUTOLOAD_PREPEND'))
 	define('PHP_ACTIVERECORD_AUTOLOAD_PREPEND',true);
 
+require 'lib/Exceptions.php';
 require 'lib/Singleton.php';
 require 'lib/Config.php';
 require 'lib/Utils.php';
@@ -19,7 +20,6 @@ require 'lib/SQLBuilder.php';
 require 'lib/Reflections.php';
 require 'lib/Inflector.php';
 require 'lib/CallBack.php';
-require 'lib/Exceptions.php';
 require 'lib/Cache.php';
 
 if (!defined('PHP_ACTIVERECORD_AUTOLOAD_DISABLE'))
