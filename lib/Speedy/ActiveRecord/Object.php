52b42c1a670958cbfa7d7b65621fdfcd7aac4fac
Zachary Quintana <zach.quintana@gmail.com>
2012-06-19 11:40:02 -0700

    Updates to AR Object

--- lib/Speedy/ActiveRecord/Object.php
+++ lib/Speedy/ActiveRecord/Object.php
@@ -26,10 +26,12 @@ class Object {
 	protected $_mixinsLoaded = false;
 	
 	
+	
+
 	/**
-	 * Checks if mixins already loaded
-	 * @return boolean
-	 */
+	* Checks if mixins already loaded
+	* @return boolean
+	*/
 	protected function _loadedMixins() {
 		return $this->_mixinsLoaded;
 	}
@@ -51,44 +53,72 @@ class Object {
 		if ($this->respondsTo('__loadMixins')) {
 			$this->_mixins	= array_merge($this->_mixins, (array) $this->__loadMixins());
 		}
-		
-		
-		foreach ($this->_mixins as $class => $options) {
-			if (is_int($class)) {
-				$class = $options;
+	
+	
+		foreach ($this->_mixins as $mixin => $options) {
+			if (is_int($mixin)) {
+				$mixin = $options;
 			}
-			
-			if (!class_exists($class)) {
+	
+			if (isset($this->_mixinObjs[$mixin])) {
 				continue;
 			}
-			
-			if (isset($this->_mixinObjs[$class])) {
+	
+			if (is_string($mixin) && class_exists($mixin)) {
+				$class = $mixin;
+			} elseif (is_string($mixin)) {
+				import($mixin);
+				$class	= \Speedy\Loader::toClass($mixin);
+			}
+	
+			if (!$class) {
 				continue;
 			}
-			
-			$instance	= new $class((is_array($options) ? $options : null));
-			
-			$this->_mixinObjs[$class] = $instance;
+	
+			$instance	= new $class($this, (is_array($options) ? $options : null));
+			$this->_addPropertiesFromMixin($instance);
+			$this->_mixinObjs[$mixin] = $instance;
 		}
-		
+	
 		$this->_mixinsLoaded = true;
 		return $this;
 	}
 	
 	/**
 	 * Attempts to call a mixin
-	 * 
+	 *
 	 */
 	protected function _callMixin($name, $args) {
 		foreach ($this->_getMixins() as $instance) {
-			if (method_exists($instance, $name)) {
+			if ($instance instanceof \Speedy\Object && $instance->respondsTo($name)) {
 				return call_user_func_array(array($instance, $name), $args);
 			}
 		}
-		
-		throw new Exception("No method exists " . get_class($this) . "#$name");
-		
-		return null;
+	
+		return parent::__call($name, $args);
+	}
+	
+	/**
+	 * Adds properties to owning class
+	 * @param object $mixin
+	 * @return object $this
+	 */
+	private function _addPropertiesFromMixin(object $mixin) {
+		$callbacks = array('before_save', 'before_create', 'before_update', 'before_validation',
+				'before_validation_on_create', 'before_validation_on_update', 'before_destroy',
+				'after_save', 'after_create', 'after_update', 'after_validation', 
+				'after_validation_on_create', 'after_validation_on_update', 'after_destory');
+	
+		foreach ($callbacks as $callback) {
+			if (isset($mixin->{$callback})) {
+				$this->{$callback} = array_merge(
+				(is_array($this->{$callback})) ? array() : $this->{$callback},
+				$mixin->{$callback}
+				);
+			}
+		}
+	
+		return $this;
 	}
 	
 	/**
@@ -99,7 +129,7 @@ class Object {
 		if (empty($this->_mixinObjs) && !$this->_loadedMixins()) {
 			$this->_loadMixins();
 		}
-		
+	
 		return ($this->_hasMixin($mixin)) ? $this->_mixinObjs[$mixin] : null;
 	}
 	
@@ -126,7 +156,7 @@ class Object {
 		if (!$this->_loadedMixins()) {
 			$this->_loadMixins();
 		}
-		
+	
 		return $this->_callMixin($name, $args);
 	}
 }
