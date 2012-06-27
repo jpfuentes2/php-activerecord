7de8cd36029d070e4469223564562af9840a0470
Koen Punt <koen@koenpunt.nl>
2012-06-27 11:38:31 +0200

    Added option for multiple model directories

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -28,9 +28,8 @@ if (!defined('PHP_ACTIVERECORD_AUTOLOAD_DISABLE'))
 
 function activerecord_autoload($class_name)
 {
-	$path = ActiveRecord\Config::instance()->get_model_directory();
-	$root = realpath(isset($path) ? $path : '.');
-
+	$paths = ActiveRecord\Config::instance()->get_model_directories();
+	$namespace_directory = '';
 	if (($namespaces = ActiveRecord\get_namespaces($class_name)))
 	{
 		$class_name = array_pop($namespaces);
@@ -39,11 +38,19 @@ function activerecord_autoload($class_name)
 		foreach ($namespaces as $directory)
 			$directories[] = $directory;
 
-		$root .= DIRECTORY_SEPARATOR . implode($directories, DIRECTORY_SEPARATOR);
+		$namespace_directory = DIRECTORY_SEPARATOR . implode($directories, DIRECTORY_SEPARATOR);
 	}
-
-	$file = "$root/$class_name.php";
-
-	if (file_exists($file))
-		require_once $file;
+	$paths = count($paths) ? $paths : array('.');
+	
+	foreach($paths as $path)
+	{
+		$root = realpath($path);
+		$file = "{$root}{$namespace_directory}/{$class_name}.php";
+		
+		if (file_exists($file))
+		{
+			require $file;
+			return;
+		}
+	}
 }
--- README.md
+++ README.md
@@ -63,14 +63,17 @@ Example:
 ```php
 ActiveRecord\Config::initialize(function($cfg)
 {
-   $cfg->set_model_directory('/path/to/your/model_directory');
-   $cfg->set_connections(
-     array(
-       'development' => 'mysql://username:password@localhost/development_database_name',
-       'test' => 'mysql://username:password@localhost/test_database_name',
-       'production' => 'mysql://username:password@localhost/production_database_name'
-     )
-   );
+    $cfg->set_model_directories(array(
+      '/path/to/your/model_directory',
+      '/some/other/path/to/your/model_directory'
+    ));
+    $cfg->set_connections(
+      array(
+        'development' => 'mysql://username:password@localhost/development_database_name',
+        'test' => 'mysql://username:password@localhost/test_database_name',
+        'production' => 'mysql://username:password@localhost/production_database_name'
+      )
+    );
 });
 ```
 
