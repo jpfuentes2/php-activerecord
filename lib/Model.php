8960f1903cfc7c8b9a2ffe4d0615e19926630a33
abhinavsaxena <abhinav.saxena@iplayup.com>
2012-09-24 16:17:32 +0530

    Change to avoid 'Undefined index' notice
    $this->__dirty[$attribute] throws up PHP Notice: Undefined index if $attribute wasn
    t modified. Using isset to avoid this notice.

--- lib/Model.php
+++ lib/Model.php
@@ -555,7 +555,7 @@ class Model
 	 */
 	public function attribute_is_dirty($attribute)
 	{
-		return $this->__dirty && $this->__dirty[$attribute] && array_key_exists($attribute, $this->attributes);
+		return $this->__dirty && isset($this->__dirty[$attribute]) && array_key_exists($attribute, $this->attributes);
 	}
 
 	/**
