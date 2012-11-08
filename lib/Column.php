5bdf159ad4f771c7164b5d02035079acb1c42678
Leighton Shank <leighton@getloaded.com>
2012-11-08 13:28:57 -0500

    Adding custom support for boolean casting based on connection
    The default PHP boolean-to-string casting does not work for some
    database connection types (e.g. PostgreSQL).  Added a BOOLEAN
    type to the Column class, and then the cast() method delegates
    to the a new boolean_to_string() method in the Connection class.
    
    By default the boolean_to_string() method returns the standard
    PHP string cast for the boolean, retaining the original behavior.
    This method can be overridden in adapter classes to provides custom
    casting of boolean values.
    
    Added an overridden boolean_to_string() method into the
    adapters/PgsqlAdapter.php class which casts the boolean values
    false and true to the strings "0" and "1" respectively.  These
    string values are then recognized by the Postgres database as
    boolean values, as opposed to the values "" and "1" generated
    by the standard PHP cast.

--- lib/Column.php
+++ lib/Column.php
@@ -18,6 +18,7 @@ class Column
 	const DATETIME	= 4;
 	const DATE		= 5;
 	const TIME		= 6;
+	const BOOLEAN	= 7;
 
 	/**
 	 * Map a type to an column type.
@@ -29,6 +30,8 @@ class Column
 		'timestamp'	=> self::DATETIME,
 		'date'		=> self::DATE,
 		'time'		=> self::TIME,
+
+		'boolean'	=> self::BOOLEAN,
 
 		'tinyint'	=> self::INTEGER,
 		'smallint'	=> self::INTEGER,
@@ -160,6 +163,7 @@ class Column
 			case self::STRING:	return (string)$value;
 			case self::INTEGER:	return static::castIntegerSafely($value);
 			case self::DECIMAL:	return (double)$value;
+			case self::BOOLEAN:	return $connection->boolean_to_string($value);
 			case self::DATETIME:
 			case self::DATE:
 				if (!$value)
