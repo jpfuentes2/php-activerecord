82afd93a15b8d262de1233e4bba43cf98c85cd54
Anther <anthrzy@gmail.com>
2013-02-06 01:25:40 -0500

    Bug fixes for finders

--- lib/Scope.php
+++ lib/Scope.php
@@ -6,50 +6,66 @@ namespace ActiveRecord;
 class Scopes
 {
 	protected $model = null;
-	protected $scopes = array();
-	public function __construct($model,$initial_scope)
+	protected $scopes = null;
+	public function __construct($model, $initial_scope)
 	{
 		$this->model = $model;
 		if($initial_scope)
 			$this->add_scope($initial_scope);
-		
+
 	}
+
 	public function add_scope($scope)
 	{
 		$this->scopes[] = new Scope($scope);
 	}
-	
-	public function __call($method,$args)
+
+	public function __call($method, $args)
 	{
 		$combined_options = array();
-		if($this->model->check_for_named_scope($method))
+
+		if($options = $this->model->check_for_named_scope($method))
 		{
 			$this->scopes[] = new Scope($this->model->check_for_named_scope($method));
 		}
 		else
 		{
-			foreach($this->scopes as $scope)
-			{
-				$scope_options = $scope->get_options();
-				$options = Model::extract_and_validate_options($scope_options);
-				var_dump($options);
-				foreach($options as $key=>$value)
-				{
-					if(array_key_exists($key,$options))
-					{
-						$combined_options[$key] = $value;
-					}
-					else
-					{
-						$combined_options[$key] = $value;
-					}
-				}
-			}
-			$args = $combined_options;
-			return $this->model->$method($args);
+			return $this->call_model_method($method, $args);
 		}
 		return $this;
 	}
+
+	protected function call_model_method($method, $args = array())
+	{
+		return call_user_func_array(array($this->model, $method), $args);
+	}
+	
+	public function find($type,$options=array())
+	{
+		if($this->scopes)
+		{
+			if($options)
+			{
+				$this->add_scope($options);
+			}
+			$args = array($type,$this->scopes->get_options());
+		}
+		return call_user_func_array(array($this->model, 'find'), $args);
+	}
+	
+	public function all($options = array())
+	{
+		return $this->find('all',$options);
+	}
+	public function first($options = array())
+	{
+		return $this->find('first',$options);
+	}
+	public function last()
+	{
+		return $this->find('last',$options);
+	}
+
 }
 class Scope
 {
