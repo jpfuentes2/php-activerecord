5d258308b36e22c08e9c768ab0a9a15f1649e97f
Anther <anthrzy@gmail.com>
2013-02-25 13:46:12 -0500

    Updated test to show that the correct data is being loaded, Updated name of the test to match the github fix convention

--- test/RelationshipTest.php
+++ test/RelationshipTest.php
@@ -82,11 +82,23 @@ class RelationshipTest extends DatabaseTest
 		$this->assert_default_has_many($this->get_relationship());
 	}
 	
-	public function test_eager_loading_three_levels_deep()
+	public function test_gh_256_eager_loading_three_levels_deep()
 	{
 		/* Before fix Undefined offset: 0 */
 		$conditions['include'] = array('events'=>array('host'=>array('events')));
-		Venue::first($conditions);
+		$venue = Venue::find(2,$conditions);
+		
+		$events = $venue->events;
+		$this->assertEquals(2,count($events));
+		$event_yeah_yeahs = $events[0];
+		$this->assertEquals('Yeah Yeah Yeahs',$event_yeah_yeahs->title);
+		
+		$event_host = $event_yeah_yeahs->host;
+		$this->assertEquals('Billy Crystal',$event_host->name);
+		
+		$bill_events = $event_host->events;
+		
+		$this->assertEquals('Yeah Yeah Yeahs',$bill_events[0]->title);
 	}
 	
 	/**
