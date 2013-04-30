d8a8815087f4ff764215f122de64f3c6c853f4fd
Leighton Shank <leighton@getloaded.com>
2013-04-30 13:51:21 -0400

    fixed indentation formatting to match existing code

--- lib/adapters/PgsqlAdapter.php
+++ lib/adapters/PgsqlAdapter.php
@@ -135,8 +135,8 @@ SQL;
 		);
 	}
 
-    public function boolean_to_string($boolean) {
-        return $boolean ? "1" : "0";
-    }
-
+	public function boolean_to_string($boolean)
+	{
+		return $boolean ? "1" : "0";
+	}
 }
