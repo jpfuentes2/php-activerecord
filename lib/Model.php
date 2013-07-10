4f209d9ca3cc94a3ff3efd3fba6e59249550c63b
Leighton Shank <leighton@getloaded.com>
2013-07-10 16:50:14 -0400

    Added tracking of changed attributes
    To complement the flagging of dirty attributes, added functionality
    similar to Rails which will track the values of changed attributes.
    
    Added the following methods:
    - $model->changed_attributes()
      Returns a hash of all attributes that have changed and their original
      values.
      #=> $changed_attributes[$attribute] = $original_value;
    
    - $model->changes()
      Returns a hash of attributes that have changed with the original and
      current values.
      #=> $changes[$attribute] = array($original_value, $current_value)
    
    - $model->previous_changes()
      Returns the changes to a model's attributes before it was saved.
    
    - $model->attribute_was($attribute)
      Returns the original value of an attribute before it was changed
    
    Also made a change to the read_attribute() method.  It seems that
    there is a quirk in PHP when the return value of the __get() method
    is passed by reference and you are trying to set a value using one
    of the 'combined assignment' operators (e.g. .=).  If the return
    value of the __get() method is a reference to an array element then
    a weird thing happens - the value passed to the __set() method
    is the same as the current value.  That's a super confusing
    explanation, so an example will clear it up:
    
    class Person {
        private $attributes = array('name' => "John");
        public function &__get($name) {
            return $this->attributes[$name];
        }
        public function __set($name, $value) {
            echo "original value: {$this->attributes[$name]}\n";
            $this->attributes[$name] = $value;
            echo "new value: {$this->attributes[$name]}\n";
        }
    }
    
    $person = new Person();
    $person->name .= " Doe";
    
    A simple change to the __get() method:
    public function &__get($name) {
        $value = $this->attributes[$name];
        return $value;
    }
    
    seems to clear up the issue.  So I applied that same logic to the
    Model::read_attribute() method, assigning the $this->attributes[$name]
    value to a variable, and then returning that.
    
    I don't think that should mess anything up, all unit tests still pass.

--- lib/Model.php
+++ lib/Model.php
@@ -88,6 +88,23 @@ class Model
 	private $attributes = array();
 
 	/**
+	 * Contains changes made to model attributes as
+	 * $attribute_name => array($original_value, $current_value)
+	 *
+	 * @var array
+	 * @access private
+	 */
+	private $changed_attributes = array();
+
+	/**
+	 * Contains changes made to model attributes before it was saved
+	 *
+	 * @var array
+	 * @access private
+	 */
+	private $previously_changed = array();
+
+	/**
 	 * Flag whether or not this model's attributes have been modified since it will either be null or an array of column_names that have been modified
 	 *
 	 * @var array
@@ -468,9 +485,21 @@ class Model
 		if ($value instanceof DateTime)
 			$value->attribute_of($this,$name);
 
-		$flag_dirty = isset($this->attributes[$name]) && ($this->attributes[$name] === $value) ? false : true;
-        $this->attributes[$name] = $value;
-		if ($flag_dirty) $this->flag_dirty($name);
+		// only update the attribute if it isn't set or has changed
+		if (!isset($this->attributes[$name]) || ($this->attributes[$name] !== $value)) {
+			// track changes to the attribute
+			if (isset($this->attributes[$name]) && !isset($this->changed_attributes[$name])) {
+				// convert DateTime objects to a string
+				if ($this->attributes[$name] instanceof DateTime)
+					$this->changed_attributes[$name] = strval($this->attributes[$name]);
+				else
+					$this->changed_attributes[$name] = $this->attributes[$name];
+			}
+
+			// set the attribute and flag as dirty
+			$this->attributes[$name] = $value;
+			$this->flag_dirty($name);
+		}
 		return $value;
 	}
 
@@ -490,8 +519,10 @@ class Model
 			$name = static::$alias_attribute[$name];
 
 		// check for attribute
-		if (array_key_exists($name,$this->attributes))
-			return $this->attributes[$name];
+		if (array_key_exists($name,$this->attributes)) {
+			$value = $this->attributes[$name];
+			return $value;
+		}
 
 		// check relationships if no attribute
 		if (array_key_exists($name,$this->__relationships))
@@ -579,6 +610,50 @@ class Model
 	public function attributes()
 	{
 		return $this->attributes;
+	}
+
+	/**
+	 * Returns a copy of the model's changed attributes hash with the
+	 * attribute name and the original value.
+	 *
+	 * @return array A copy of the model's changed attribute data
+	 */
+	public function changed_attributes()
+	{
+		return $this->changed_attributes;
+	}
+
+	/**
+	 * Returns a copy of the model's changed attributes as a hash
+	 * in the form $attribute => array($original_value, $current_value)
+	 *
+	 * @return array A copy of the model's attribute changes
+	 */
+	public function changes()
+	{
+		return array_merge_recursive(
+			$this->changed_attributes,
+			array_intersect_key($this->attributes, $this->changed_attributes)
+		);
+	}
+
+	/**
+	 * Returns a copy of the model's changed attributes before it was saved
+	 *
+	 * @return array A copy of the model's changed attribute before a save
+	 */
+	public function previous_changes()
+	{
+		return $this->previously_changed;
+	}
+
+	/**
+	 * Returns the value of an attribute before it was changed
+	 *
+	 * @return string The original value of an attribute
+	 */
+	public function attribute_was($name) {
+		return isset($this->changed_attributes[$name]) ? $this->changed_attributes[$name] : null;
 	}
 
 	/**
@@ -1305,9 +1380,11 @@ class Model
 	 *
 	 * @see dirty_attributes
 	 */
-	public function reset_dirty()
+	public function reset_dirty($model_was_saved=false)
 	{
 		$this->__dirty = null;
+		$this->previously_changed = $model_was_saved ? $this->changes() : array();
+		$this->changed_attributes = array();
 	}
 
 	/**
