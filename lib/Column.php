2b117024b619e1abf29ba2b2a3ef288139a8aa75
Trevor Suarez <rican7@gmail.com>
2014-02-27 11:08:58 -0500

    Conforming to the block style set already for these classes

--- lib/Column.php
+++ lib/Column.php
@@ -121,25 +121,24 @@ class Column
 	 */
 	public static function castIntegerSafely($value)
 	{
-		if (is_int($value)) {
+		if (is_int($value))
 			return $value;
-		}
+
 		// Its just a decimal number
-		elseif (is_numeric($value) && floor($value) != $value) {
+		elseif (is_numeric($value) && floor($value) != $value)
 			return (int) $value;
-		}
+
 		// If adding 0 to a string causes a float conversion,
 		// we have a number over PHP_INT_MAX
-		elseif (is_string($value) && is_float($value + 0)) {
+		elseif (is_string($value) && is_float($value + 0))
 			return (string) $value;
-		}
+
 		// If a float was passed and its greater than PHP_INT_MAX
 		// (which could be wrong due to floating point precision)
 		// We'll also check for equal to (>=) in case the precision
 		// loss creates an overflow on casting
-		elseif (is_float($value) && $value >= PHP_INT_MAX) {
+		elseif (is_float($value) && $value >= PHP_INT_MAX)
 			return number_format($value, 0, '', '');
-		}
 
 		return (int) $value;
 	}
