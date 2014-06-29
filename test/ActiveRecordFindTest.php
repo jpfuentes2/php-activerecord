81a6e22076901005a18c5a60a1142bfd69bb2605
Max Loeb <shmax@shmax.com>
2014-06-29 15:53:33 -0700

    model-cache

--- test/ActiveRecordFindTest.php
+++ test/ActiveRecordFindTest.php
@@ -79,6 +79,15 @@ class ActiveRecordFindTest extends DatabaseTest
 		$this->assert_equals(1,$authors[0]->author_id);
 	}
 
+	/**
+	 * @expectedException ActiveRecord\DatabaseException
+	 */
+	public function test_find_all_with_empty_array_bind_value_throws_exception()
+	{
+		$authors = Author::find('all',array('conditions' => array('author_id IN(?)', array())));
+		$this->assertCount(0,$authors);
+	}
+
 	public function test_find_hash_using_alias()
 	{
 		$venues = Venue::all(array('conditions' => array('marquee' => 'Warner Theatre', 'city' => array('Washington','New York'))));
