7d02b6f47cdee1932716c199ff6901027f64ebb4
Koen Punt <koen@koenpunt.nl>
2014-07-20 13:15:29 +0200

    fix indentation

--- lib/Serialization.php
+++ lib/Serialization.php
@@ -340,32 +340,32 @@ class XmlSerializer extends Serialization
  */
 class CsvSerializer extends Serialization
 {
-  public static $delimiter = ',';
-  public static $enclosure = '"';
+	public static $delimiter = ',';
+	public static $enclosure = '"';
 
-  public function to_s()
-  {
-    if (@$this->options['only_header'] == true) return $this->header();
-    return $this->row();
-  }
+	public function to_s()
+	{
+		if (@$this->options['only_header'] == true) return $this->header();
+		return $this->row();
+	}
 
-  private function header()
-  {
-    return $this->to_csv(array_keys($this->to_a()));
-  }
+	private function header()
+	{
+		return $this->to_csv(array_keys($this->to_a()));
+	}
 
-  private function row()
-  {
-    return $this->to_csv($this->to_a());
-  }
+	private function row()
+	{
+		return $this->to_csv($this->to_a());
+	}
 
-  private function to_csv($arr)
-  {
-    $outstream = fopen('php://temp', 'w');
-    fputcsv($outstream, $arr, self::$delimiter, self::$enclosure);
-    rewind($outstream);
-    $buffer = trim(stream_get_contents($outstream));
-    fclose($outstream);
-    return $buffer;
-  }
+	private function to_csv($arr)
+	{
+		$outstream = fopen('php://temp', 'w');
+		fputcsv($outstream, $arr, self::$delimiter, self::$enclosure);
+		rewind($outstream);
+		$buffer = trim(stream_get_contents($outstream));
+		fclose($outstream);
+		return $buffer;
+	}
 }
