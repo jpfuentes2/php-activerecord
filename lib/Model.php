006445537ac8c1a6f8660c7c5e956fa638796606
Koen Punt <koen@koenpunt.nl>
2014-12-04 23:44:06 +0100

    store cache on first fetch, expire on update

--- lib/Model.php
+++ lib/Model.php
@@ -154,14 +154,16 @@ class Model
 	static $sequence;
 
 	/**
-	 * Set this to true in your subclass to use caching for this model. Note that you must also configure a cache object.
+	 * Set this to true in your subclass to use caching for this model.
+	 * Note that you must also configure a cache object.
 	 */
 	static $cache = false;
 
 	/**
-	 * Set this to specify an expiration period for this model. If not set, the expire value you set in your cache options will be used.
+	 * Set this to specify an expiration period for this model.
+	 * If not set, the expire value you set in your cache options will be used.
 	 *
-	 * @var number
+	 * @var integer
 	 */
 	static $cache_expire;
 
@@ -847,7 +849,7 @@ class Model
 		$this->__new_record = false;
 		$this->invoke_callback('after_create',false);
 
-		$this->update_cache();
+		$this->expire_cache();
 		return true;
 	}
 
@@ -878,17 +880,18 @@ class Model
 			$dirty = $this->dirty_attributes();
 			static::table()->update($dirty,$pk);
 			$this->invoke_callback('after_update',false);
-			$this->update_cache();
+			$this->expire_cache();
 		}
 
 		return true;
 	}
 
-	protected function update_cache()
+	protected function expire_cache()
 	{
 		$table = static::table();
-		if($table->cache_individual_model){
-			Cache::set($this->cache_key(), $this, $table->cache_model_expire);
+		if($table->cache_individual_model)
+		{
+			Cache::delete($this->cache_key());
 		}
 	}
 
@@ -1032,18 +1035,9 @@ class Model
 
 		static::table()->delete($pk);
 		$this->invoke_callback('after_destroy',false);
-		$this->remove_from_cache();
+		$this->expire_cache();
 
 		return true;
-	}
-
-	public function remove_from_cache()
-	{
-		$table = static::table();
-		if($table->cache_individual_model)
-		{
-			Cache::delete($this->cache_key());
-		}
 	}
 
 	/**
@@ -1279,13 +1273,12 @@ class Model
 	 */
 	public function reload()
 	{
-		$this->remove_from_cache();
-
 		$this->__relationships = array();
 		$pk = array_values($this->get_values_for($this->get_primary_key()));
 
 		$this->set_attributes_via_mass_assignment($this->find($pk)->attributes, false);
 		$this->reset_dirty();
+		$this->expire_cache();
 
 		return $this;
 	}
@@ -1617,14 +1610,19 @@ class Model
 	 * @param array $pks An array of primary keys
 	 * @return array
 	 */
-	protected static function get_models_from_cache(array $pks)
+	protected static function get_models_from_cache($pks)
 	{
 		$models = array();
 		$table = static::table();
 
+		if(!is_array($pks))
+		{
+			$pks = array($pks);
+		}
+
 		foreach($pks as $pk)
 		{
-			$options =array('conditions' => static::pk_conditions($pk));
+			$options = array('conditions' => static::pk_conditions($pk));
 			$models[] = Cache::get($table->cache_key_for_model($pk), function() use ($table, $options)
 			{
 				$res = $table->find($options);
@@ -1649,8 +1647,7 @@ class Model
 
 		if($table->cache_individual_model)
 		{
-			$pks = is_array($values) ? $values : array($values);
-			$list = static::get_models_from_cache($pks);
+			$list = static::get_models_from_cache($values);
 		}
 		else
 		{
