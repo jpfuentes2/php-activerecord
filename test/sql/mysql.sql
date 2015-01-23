1d82493f5c3c53fde0f455f8eb8934fe43e3e6a2
Trevor Suarez <rican7@gmail.com>
2015-01-23 15:34:50 -0500

    Wrapping the table names in backticks

--- test/sql/mysql.sql
+++ test/sql/mysql.sql
@@ -2,7 +2,7 @@
 SET SESSION sql_mode = 'NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES'
 
 
-CREATE TABLE authors(
+CREATE TABLE `authors` (
 	author_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	parent_author_id INT,
 	name VARCHAR(25) NOT NULL DEFAULT 'default_name',
@@ -16,7 +16,7 @@ CREATE TABLE authors(
 	mixedCaseField varchar(50)
 ) ENGINE=InnoDB;
 
-CREATE TABLE books(
+CREATE TABLE `books` (
 	book_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	Author_Id INT,
 	secondary_author_id INT,
@@ -25,12 +25,12 @@ CREATE TABLE books(
 	special NUMERIC(10,2) DEFAULT 0
 );
 
-CREATE TABLE publishers(
+CREATE TABLE `publishers` (
 	publisher_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(25) NOT NULL DEFAULT 'default_name'
 ) ENGINE=InnoDB;
 
-CREATE TABLE venues (
+CREATE TABLE `venues` (
 	Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name varchar(50),
 	city varchar(60),
@@ -40,7 +40,7 @@ CREATE TABLE venues (
 	UNIQUE(name,address)
 );
 
-CREATE TABLE events (
+CREATE TABLE `events` (
 	id int NOT NULL auto_increment PRIMARY KEY,
 	venue_id int NULL,
 	host_id int NOT NULL,
@@ -49,19 +49,19 @@ CREATE TABLE events (
 	type varchar(15) default NULL
 );
 
-CREATE TABLE hosts(
+CREATE TABLE `hosts` (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(25)
 );
 
-CREATE TABLE employees (
+CREATE TABLE `employees` (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	first_name VARCHAR(255) NOT NULL,
 	last_name VARCHAR(255) NOT NULL,
 	nick_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE positions (
+CREATE TABLE `positions` (
 	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	employee_id int NOT NULL,
 	title VARCHAR(255) NOT NULL,
@@ -74,42 +74,42 @@ CREATE TABLE `rm-bldg`(
     `space out` VARCHAR(1) NOT NULL
 );
 
-CREATE TABLE awesome_people(
+CREATE TABLE `awesome_people` (
 	id int not null primary key auto_increment,
 	author_id int,
 	is_awesome int default 1
 );
 
-CREATE TABLE amenities(
+CREATE TABLE `amenities` (
   `amenity_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `type` varchar(40) NOT NULL DEFAULT ''
 );
 
-CREATE TABLE property(
+CREATE TABLE `property` (
   `property_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
-CREATE TABLE property_amenities(
+CREATE TABLE `property_amenities` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `amenity_id` int(11) NOT NULL DEFAULT '0',
   `property_id` int(11) NOT NULL DEFAULT '0'
 );
 
-CREATE TABLE users (
+CREATE TABLE `users` (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 ) ENGINE=InnoDB;
 
-CREATE TABLE newsletters (
+CREATE TABLE `newsletters` (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 ) ENGINE=InnoDB;
 
-CREATE TABLE user_newsletters (
+CREATE TABLE `user_newsletters` (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     newsletter_id INT NOT NULL
 ) ENGINE=InnoDB;
 
-CREATE TABLE valuestore (
+CREATE TABLE `valuestore` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `key` varchar(20) NOT NULL DEFAULT '',
   `value` varchar(255) NOT NULL DEFAULT ''
