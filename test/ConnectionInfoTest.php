5a6f015a5ee8f8333e5b0f8303d75ebe3671e740
Koen Punt <koen@koenpunt.nl>
2015-02-10 00:36:39 +0100

    assert database name for unix socket

--- test/ConnectionInfoTest.php
+++ test/ConnectionInfoTest.php
@@ -15,12 +15,12 @@ class ConnectionInfoTest extends SnakeCase_PHPUnit_Framework_TestCase
 	public function test_connection_info()
 	{
 		$info = ConnectionInfo::from_connection_url('mysql://user:pass@127.0.0.1:3306/dbname');
-		$this->assert_equals('mysql',$info->protocol);
-		$this->assert_equals('user',$info->username);
-		$this->assert_equals('pass',$info->password);
-		$this->assert_equals('127.0.0.1',$info->host);
-		$this->assert_equals(3306,$info->port);
-		$this->assert_equals('dbname',$info->database);
+		$this->assert_equals('mysql', $info->protocol);
+		$this->assert_equals('user', $info->username);
+		$this->assert_equals('pass', $info->password);
+		$this->assert_equals('127.0.0.1', $info->host);
+		$this->assert_equals(3306, $info->port);
+		$this->assert_equals('dbname', $info->database);
 	}
 	
 	public function test_gh_103_sqlite_connection_string_relative()
@@ -58,14 +58,15 @@ class ConnectionInfoTest extends SnakeCase_PHPUnit_Framework_TestCase
 	public function test_parse_connection_url_with_unix_sockets()
 	{
 		$info = ConnectionInfo::from_connection_url('mysql://user:password@unix(/tmp/mysql.sock)/database');
-		$this->assert_equals('/tmp/mysql.sock',$info->host);
+		$this->assert_equals('/tmp/mysql.sock', $info->host);
+		$this->assert_equals('database', $info->database);
 	}
 
 	public function test_parse_connection_url_with_decode_option()
 	{
 		$info = ConnectionInfo::from_connection_url('mysql://h%20az:h%40i@127.0.0.1/test?decode=true');
-		$this->assert_equals('h az',$info->username);
-		$this->assert_equals('h@i',$info->password);
+		$this->assert_equals('h az', $info->username);
+		$this->assert_equals('h@i', $info->password);
 	}
 
 	public function test_encoding()
@@ -83,12 +84,12 @@ class ConnectionInfoTest extends SnakeCase_PHPUnit_Framework_TestCase
 			'username' => 'user',
 			'password' => 'pass'
 		));
-		$this->assert_equals('mysql',$info->protocol);
-		$this->assert_equals('user',$info->username);
-		$this->assert_equals('pass',$info->password);
-		$this->assert_equals('127.0.0.1',$info->host);
-		$this->assert_equals(3306,$info->port);
-		$this->assert_equals('dbname',$info->database);
+		$this->assert_equals('mysql', $info->protocol);
+		$this->assert_equals('user', $info->username);
+		$this->assert_equals('pass', $info->password);
+		$this->assert_equals('127.0.0.1', $info->host);
+		$this->assert_equals(3306, $info->port);
+		$this->assert_equals('dbname', $info->database);
 	}
 
 }
