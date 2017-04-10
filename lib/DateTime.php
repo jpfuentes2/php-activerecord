222960fea5dd0d2bb8519d8cd3184744939bd658
Mattias Hising <hising@gmail.com>
2017-04-10 15:58:46 +0200

    Add microseconds to setTime to support PHP 7.1

--- lib/DateTime.php
+++ lib/DateTime.php
@@ -165,10 +165,10 @@ class DateTime extends \DateTime implements DateTimeInterface
 		return parent::setISODate($year, $week, $day);
 	}
 
-	public function setTime($hour, $minute, $second = 0)
+	public function setTime($hour, $minute, $second = 0, $microseconds = 0)
 	{
 		$this->flag_dirty();
-		return parent::setTime($hour, $minute, $second);
+        return parent::setTime($hour, $minute, $second);
 	}
 
 	public function setTimestamp($unixtimestamp)
