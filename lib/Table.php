2c6bee77dee49e53474165418f4e1216d29e99b1
Brian Muse <brian.muse@gmail.com>
2017-06-03 07:43:11 -0400

    Invalid index when including empty array as nested includes. (#560)
    * Fixing issue with invalid index when nested includes is an empty array/
    
    * Adds test
    
    * Adjusting tabs and spacing

--- lib/Table.php
+++ lib/Table.php
@@ -286,7 +286,7 @@ class Table
 			// nested include
 			if (is_array($name))
 			{
-				$nested_includes = count($name) > 0 ? $name : $name[0];
+				$nested_includes = count($name) > 0 ? $name : array();
 				$name = $index;
 			}
 			else
