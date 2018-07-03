172215408cb441f1ea8084c1cd1d131981084e25
Ennio Wolsink <ennio@rimote.nl>
2018-07-03 17:00:43 +0200

    Fix parse error in Column.php
    hotfix ready for release

--- lib/Column.php
+++ lib/Column.php
@@ -130,8 +130,7 @@ class Column
 
 		// If adding 0 to a numeric string that is not a decimal causes a float conversion,
 		// we have a number over PHP_INT_MAX
-		elseif (is_string($value) 
-			&& is_numeric($value) && is_float($value + 0)) {
+		elseif (is_string($value) && is_numeric($value) && is_float($value + 0)) 
 			return (string) $value;
 
 		// If a float was passed and its greater than PHP_INT_MAX
