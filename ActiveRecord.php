e19ff51fbb83bb80533865fd5d4948ab9ac65841
Shane Iseminger <shane@shaneiseminger.com>
2021-04-16 10:44:46 -0600

    spl_autoload_register() will always throw now

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -25,7 +25,7 @@ require __DIR__.'/lib/Exceptions.php';
 require __DIR__.'/lib/Cache.php';
 
 if (!defined('PHP_ACTIVERECORD_AUTOLOAD_DISABLE'))
-	spl_autoload_register('activerecord_autoload',false,PHP_ACTIVERECORD_AUTOLOAD_PREPEND);
+	spl_autoload_register('activerecord_autoload',true,PHP_ACTIVERECORD_AUTOLOAD_PREPEND);
 
 function activerecord_autoload($class_name)
 {
