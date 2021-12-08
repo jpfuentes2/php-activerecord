fbc7f3aa0453cb0c2d6e2869fbe1a122b0f7e5b2
AgeWap <agewap@gmail.com>
2021-12-08 20:08:08 +0800

    fix php 8 getIterator()

--- lib/Validations.php
+++ lib/Validations.php
@@ -517,7 +517,7 @@ class Validations
 						$message = $options['message'];
 					else
 						$message = $options[$messageOptions[$range_option]];
-					
+
 
 					$message = str_replace('%d', $option, $message);
 					$attribute_value = $this->model->$attribute;
@@ -904,6 +904,7 @@ class Errors implements IteratorAggregate
 	 *
 	 * @return ArrayIterator
 	 */
+	#[\ReturnTypeWillChange]
 	public function getIterator()
 	{
 		return new ArrayIterator($this->full_messages());
