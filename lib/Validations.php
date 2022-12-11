0fab96639bffbfef0de1177eb5c42baf89ca89a0
AgeWap <agewap@gmail.com>
2022-12-12 02:50:19 +0800

    php8.2 fix

--- lib/Validations.php
+++ lib/Validations.php
@@ -45,6 +45,7 @@ class Validations
 	private $options = [];
 	private $validators = [];
 	private $record;
+	private ?string $klass;
 
 	private static array $VALIDATION_FUNCTIONS = [
 		'validates_presence_of',
